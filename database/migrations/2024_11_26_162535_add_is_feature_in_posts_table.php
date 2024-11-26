16d48f48577ee7f6c80e189093b68b2595b2633b
TsengJay <tsengjay00@>
2024-11-27 00:28:24 +0800

    運用 migration 調整資料表結構

---
+++ database/migrations/2024_11_26_162535_add_is_feature_in_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title', 500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title')->change();
+            $table->dropColumn('is_feature');
+        });
+    }
+    
+};
