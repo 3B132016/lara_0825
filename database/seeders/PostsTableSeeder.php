d71792b9a22f9ad3b08146865390b3845257b786
3B132016 <tsengjay0gmail.com>
2025-01-02 22:53:42 +0800

    撰寫 Seeder 內容

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Post;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -10,8 +11,17 @@ class PostsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        foreach (range(1, 20) as $number) {
+            Post::create([
+                'title' => 'title ' . $number,
+                'content' => 'content ' . $number,
+            ]);
+        }
     }
 }
+
+
+
+
